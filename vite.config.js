09b7d7f54240eb3b7f7ebffe73d7b0cee6ab899f
Raj <112654432+RajMasihi@users.noreply.github.com>
2025-05-14 14:31:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.js'),
-      name: 'ReactUIComponents form and card by raj',
+      name: 'ReactUIComponentsFormAndCard',
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {

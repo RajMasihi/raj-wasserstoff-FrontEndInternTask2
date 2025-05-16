a4779d8d2bee52b1dce05a59ae2567af581a42fd
Raj Masihi <rajmasihi6397@gmail.com>
2025-05-17 02:20:07 +0530

    add css into vite config and run build

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
+
 export { default as Form } from "./src/components/form/Form";
 export { default as Displaycard } from "./src/components/displaycard/Displaycard";
--- vite.config.js
+++ vite.config.js
@@ -7,16 +7,13 @@ export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-<<<<<<< HEAD
+
       entry: path.resolve(__dirname, 'index.js'),
       name: 'ReactUIComponentsFormcardByRaj',
       formats: ['es', 'umd'],
       fileName: (format) => `index.${format}.js`,
-=======
-      entry: path.resolve(__dirname, 'src/index.js'),
-      name: 'ReactUIComponentsFormAndCard',
-      fileName: (format) => `raj-wasserstoff-FrontEndInternTask2.${format}.js`,
->>>>>>> df98461d0ce6c7d4e6302327d2c3b0a9f83e4486
+
+   
     },
     rollupOptions: {
       
